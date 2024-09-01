20d452622031223b5aedd0aaaa970616756bcff8
Doe Agudey Daniel <agudeydaniel8@gmail.com>
2024-09-01 01:04:30 +0000

    ai based weather app

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as sl
+import streamlit.components.v1 as com
+
+from weatherapp import app 
+
+def get_weather(user_input):
+    input = {"messages": [user_input]}
+    response = app.invoke(input)
+    sl.session_state["response"] = response
+    
+
+with sl.container():
+    col1, col2 = sl.columns([1,2])
+
+    with col1:
+        com.iframe("https://lottie.host/embed/27cd04af-f29d-4e64-91bd-b6e2040b8945/qWOrnKhKIy.json")
+        
+
+    with col2:
+        sl.title('Global City Weather Checker')
+        
+
+sl.markdown("---")
+
+with sl.form(key='weather_form', clear_on_submit=True):
+    user_input=sl.text_input('Enter prompt to obtain weather information about a City', max_chars=60,key='user_input')
+    submit_button = sl.form_submit_button(label='Get Weather')
+
+    
+
+if submit_button:
+    response_message = get_weather(user_input)
+
+    sl.markdown("<h3 style='color: #4CAF50;'>🌤️ Weather Information</h3>", unsafe_allow_html=True) 
+    sl.markdown(f"""
+        <div style='padding: 15px; border: 2px solid #4CAF50; border-radius: 10px;'>
+            <p style='font-size: 18px; ;'><strong>{sl.session_state['response']}</strong></p>
+        </div>
+    """, unsafe_allow_html=True)
+
+
---
+++ weatherapp.py
@@ -0,0 +1,87 @@
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+from langchain.agents import Tool
+from langchain.tools.base import BaseTool
+from langgraph.graph import Graph
+import requests
+
+
+#AI Model
+
+load_dotenv()
+
+os.environ['GROQ_API_KEY'] = os.environ.get("GROQ_API_KEY")
+
+model = ChatGroq(
+    model="mixtral-8x7b-32768",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+
+##Weather tool
+class TomorrowIOWeatherTool(BaseTool):
+    name = "tomorrow_io_weather"
+    description = "Fetch weather forecasts from the Tomorrow.io API"
+
+    def _run(self, location: str):
+        url = "https://api.tomorrow.io/v4/timelines"
+        querystring = {
+            "location": location,
+            "fields": ["temperature", "cloudCover"],
+            "units": "imperial",
+            "timesteps": "1d",
+            "apikey": os.environ["TOMORROWDOTIO_API_KEY"]
+        }
+        response = requests.request("GET", url, params=querystring)
+        return response.text
+
+    async def _arun(self, location: str):
+        return self._run(location)
+
+
+## functions which serve as nodes
+def get_req(state):
+    messages = state['messages']
+    user_input = messages[-1]
+    complete_query ="Your task is to provide only the city name based on the user input only, Nothing more, just the city name mentioned. Following is the user query: " + user_input
+    response = model.invoke(complete_query)
+    state['messages'].append(response.content)
+    return state
+
+def findWeather(state):
+    messages = state['messages']
+    agent_response = messages[-1]
+    weather = TomorrowIOWeatherTool()
+    weather_data = weather.run(agent_response)
+    state['messages'].append(weather_data)
+    return state
+
+def filter_res(state):
+    messages = state['messages']
+    user_input = messages[0]
+    available_info = messages[-1]
+    agent2_query = "Your task is to provide info concisely based on the user query and the available information from the internet and format it with creative emojis and well structured. \
+                     following is the user query: " + user_input + "Available information: " + available_info
+    response = model.invoke(agent2_query)
+    return response.content
+
+## Langraph initialization
+workflow = Graph()
+
+workflow.add_node('agent', get_req)
+workflow.add_node('weather tool', findWeather)
+workflow.add_node('responder', filter_res)
+
+workflow.add_edge('agent', 'weather tool')
+workflow.add_edge('weather tool', 'responder')
+
+workflow.set_entry_point('agent')
+workflow.set_finish_point('responder')
+
+app = workflow.compile()
+
+
